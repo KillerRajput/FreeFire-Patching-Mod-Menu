39584b7c5032e3c5dc467b45b13c7966359b16b4
Anmol Ratan <63386088+KillerRajput@users.noreply.github.com>
2021-11-26 10:51:40 +0530

    Update MainActivity.java

--- app/src/main/java/mpg/team/ff/MainActivity.java
+++ app/src/main/java/mpg/team/ff/MainActivity.java
@@ -4,10 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+      public String GameActivity = "com.dts.freefireth.FFMainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText((Context)this, "Modded By Thakur Sahab (Anmol Singh)", Toast.LENGTH_LONG).show();
         StaticActivity.Start(this);
+    try {
+      startActivity(new Intent((Context)this, Class.forName(this.GameActivity)));
+      return;
+    } catch (ClassNotFoundException classNotFoundException) {
+      classNotFoundException.printStackTrace();
+      return;
+    }
     }
 }
